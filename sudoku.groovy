33749e9ec3761958e3eaddb6dbbdbd5ef46bfb30
fumokmm <fumo.kmm@gmail.com>
2013-02-24 15:51:29 +0900

    初期状態と終了状態の出力

--- sudoku.groovy
+++ sudoku.groovy
@@ -19,6 +19,7 @@ class Observer {
 // http://bit.ly/15caX31
 // の「ナンプレ超初級問題１」固定で。
 
+// 全セルを定義
 def allCells = [
   new Cell(x: 0, y: 0, fixed: 0, candidate: []),
   new Cell(x: 1, y: 0, fixed: 0, candidate: []),
@@ -37,3 +38,21 @@ def allCells = [
   new Cell(x: 2, y: 3, fixed: 0, candidate: []),
   new Cell(x: 3, y: 3, fixed: 0, candidate: [])
 ]
+
+/** 結果出力 */
+def output(cells) {
+  (0..3).each { y ->
+    (0..3).each { x ->
+      print( cells.find { it.x == x && it.y == y }.fixed )
+    }
+    println()
+  }
+}
+
+println '----- 開始 -----'
+output(allCells)
+
+// ここで解く
+
+println '----- 終了 -----'
+output(allCells)
