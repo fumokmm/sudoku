f2a86f74ceda1614ee56fdaf0cd446cf6f5fd361
areph <40balmung@gmail.com>
2013-02-24 15:58:36 +0900

    Observer実装中

--- sudoku.groovy
+++ sudoku.groovy
@@ -10,4 +10,23 @@ class Cell {
 }
 
 class Observer {
+  // 監視するセルを宣言
+  def cells = []
+  // 決定済みの値
+  def fixeds = []
+  def allVal = 1..cells.size
+
+  // 監視している全てのセルで決定済みの値を取得する
+  def fix() {
+    fixeds = cells.findAll(cells.fixed != 0)
+  }
+      
+  // 監視しているセルに対して候補を埋める
+  def calc() {
+    cells.findAll(cells.fixed == 0).each() {
+      // TODO candidateが存在する場合はAnd演算
+      // 候補を決定
+      it.candidate = allVal - fixeds
+    }
+  }
 }
